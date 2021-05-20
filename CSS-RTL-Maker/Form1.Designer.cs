58c5c36c2a2e1968230d65d66741d73710dc3391
MSJM <se.saleemalsaleh@gmail.com>
2021-05-20 23:04:21 +0300

    Add project files.

---
+++ CSS-RTL-Maker/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CSSRTLMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(390, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 23);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(365, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(174, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Convert To RTL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Css Files | *.css";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(506, 106);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Simplified Arabic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CSS-RTL-Maker / se.saleemalsaleh@gmail.com";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ CSS-RTL-Maker/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSSRTLMaker
+{
+    public partial class Form1 : Form
+    {
+        string targetpath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                string filename = Path.GetFileNameWithoutExtension(textBox1.Text);
+                targetpath = Path.GetDirectoryName(openFileDialog1.FileName) + "\\"+filename+"-rtl.css";
+                button2.Enabled = true;
+            }
+            else
+            {
+                textBox1.Clear();
+                button2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            convertToRTL(textBox1.Text);
+        }
+
+        private void convertToRTL(string Filename)
+        {
+            List<string> fileLines = File.ReadAllLines(Filename).ToList();
+
+           
+            for (int i = 0; i < fileLines.Count; i++)
+            {
+                string ln = fileLines[i];
+                if (endswithsemi(ln))
+                {
+                    if (ContainsWord(ln, "right"))
+                    {
+                        fileLines[i] = ln.Replace("right", "left");
+                    }
+                    else if (ContainsWord(ln, "left"))
+                    {
+                        fileLines[i] = ln.Replace("left", "right");
+                    }
+
+                    if (ContainsWord(ln, "translatex"))
+                    {
+                        if (ContainsWord(ln.Substring(ln.IndexOf("(")), "-"))
+                        {
+                            //string x= ln.Substring(0, ln.IndexOf("(")) + (ln.Substring(ln.IndexOf("(")).Replace("-", ""));
+                            fileLines[i] = ln.Substring(0,ln.IndexOf("("))+(ln.Substring(ln.IndexOf("(")).Replace("-", ""));
+                        }
+                        else
+                        {
+                            fileLines[i] = ln.Replace("(", "(-");
+                        }
+                    }
+                }
+            }
+
+         
+
+            Saveconverted(targetpath, fileLines);
+
+        }
+
+
+
+        private void Saveconverted(string targetpath, List<string> fileLines)
+        {
+            try
+            {
+                File.WriteAllLines(targetpath, fileLines);
+                MessageBox.Show("Export Converted File Succedded");
+                string dir = Path.GetDirectoryName(targetpath);
+                Process.Start(dir);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("error : " + e.Message);
+            }
+
+        }
+
+
+        private bool ContainsWord(string ln, string word)
+        {
+            return ln.ToLower().Contains(word);
+        }
+
+        private bool endswithsemi(string ln)
+        {
+
+
+            return ln.Length > 0 ? ln.ElementAt(ln.Length - 1).Equals(';') : false;
+
+        }
+    }
+}
